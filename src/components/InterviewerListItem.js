dd81d0ee7898746e0e97079515784738e3d66464
Liam <liam.s.smith@gmail.com>
2022-05-18 11:03:07 -0400

    create InterviewerListItem component and scss file'

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem({
+  selected,
+  id,
+  setInterviewer,
+  avatar,
+  name,
+}) {
+  const interviewerClass = classNames("interviewers__item", {
+    " interviewers__item--selected ": selected,
+  });
+
+  return (
+    <li className={interviewerClass} onClick={() => setInterviewer(id)}>
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        // for seo and accessibility reasons
+        alt={name}
+      />
+      {/* conditional rendering of name, using && what is on the right will execute if what is on the left is true */}
+      {selected && name}
+    </li>
+  );
+}
