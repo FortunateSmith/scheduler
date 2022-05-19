3f8fb5f7091a9ff839f6d391bf6d79a27885cb72
Liam <liam.s.smith@gmail.com>
2022-05-18 23:10:10 -0400

    create Error and Status components, add stories to index.js storybook

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,8 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 // Button Stories
 storiesOf("Button", module)
@@ -161,4 +163,13 @@ storiesOf("Appointment", module)
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
     />
-  ));
+  ))
+  .add("Status", () => (
+    <Status message="Deleting" />
+  ))
+  .add("Error", () => (
+    <Error
+      message="Could not delete appointment"
+      onClose={action("onClose")}
+      />
+  ))
