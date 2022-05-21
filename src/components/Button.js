db30659d62f2a8d54dc3c08dde5fcb28eebeb29f
Liam <liam.s.smith@gmail.com>
2022-05-20 21:36:09 -0400

    format file

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,14 +3,14 @@ import "components/Button.scss";
 import classNames from "classnames";
 
 export default function Button(props) {
-
+   
    let buttonClass = classNames({
       "button": true,
       " button--danger": props.danger,
       " button--confirm": props.confirm
 })
  
-   return <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>;
+   return <button className={buttonClass} onSubmit={props.onClick} disabled={props.disabled}>{props.children}</button>;
  }
 
  
