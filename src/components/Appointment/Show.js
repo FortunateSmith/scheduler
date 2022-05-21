5959dae084001f80c161713b93181fcbb9cf8003
Liam <liam.s.smith@gmail.com>
2022-05-20 21:35:39 -0400

    create Appointment Show and index components

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -2,37 +2,35 @@ import React from "react";
 import "./styles.scss";
 
 export default function Show(props) {
-
   const edit = () => {
     props.onEdit("onEdit");
-  }
+  };
 
-  return(
+  return (
     <main className="appointment__card appointment__card--show">
-  <section className="appointment__card-left">
-    <h2 className="text--regular">{props.student}</h2>
-    <section className="interviewer">
-      <h4 className="text--light">Interviewer</h4>
-      <h3 className="text--regular">{props.interviewer.name}</h3>
-    </section>
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <img
-        className="appointment__actions-button"
-        src="images/edit.png"
-        alt="Edit"
-        onClick={edit}
-      />
-      <img
-        className="appointment__actions-button"
-        src="images/trash.png"
-        alt="Delete"
-        onClick={() => props.onDelete("10")}
-      />
-    </section>
-  </section>
-</main>
-
-  )
+      <section className="appointment__card-left">
+        <h2 className="text--regular">{props.student}</h2>
+        <section className="interviewer">
+          <h4 className="text--light">Interviewer</h4>
+          <h3 className="text--regular">{props.interviewer.name}</h3>
+        </section>
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <img
+            className="appointment__actions-button"
+            src="images/edit.png"
+            alt="Edit"
+            onClick={edit}
+          />
+          <img
+            className="appointment__actions-button"
+            src="images/trash.png"
+            alt="Delete"
+            onClick={() => props.onDelete("10")}
+          />
+        </section>
+      </section>
+    </main>
+  );
 }
