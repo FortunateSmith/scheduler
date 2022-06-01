58460898f7105388b403e9b24b3f3ebb82a37207
Liam Smith <liam.s.smith@gmail.com>
2022-06-01 17:39:48 +0000

    update imports, add custom hook for rendering components, add comments for clarity

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,22 +2,41 @@ import React from "react";
 
 import "./styles.scss";
 
+// CHILD COMPONENTS
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+///////////////////////
+
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
-  const { interview, time } = props;
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  // ternary is used to set initial state of mode
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
+  
 
   return (
     <article className="appointment">
-      <Header time={time} />
+      <Header time={props.time} />
 
-      {interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show student={props.interview.student} interviewer={props.interview.interviewer} />
+      )}
+      {mode === CREATE && (<Form interviewers={[]} 
+       onCancel={back}
+      />)}
+
+      {/* CODE BEING REPLACED:: {interview ? (
         <Show student={interview.student} interviewer={interview.interviewer} />
       ) : (
         <Empty />
-      )}
+      )} */}
     </article>
   );
 }
