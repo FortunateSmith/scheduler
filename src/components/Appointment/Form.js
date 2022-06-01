22df7fde2869daf32d06c875e586a12dad1bec26
Liam Smith <liam.s.smith@gmail.com>
2022-06-01 17:38:26 +0000

    add console.logs for testing

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,9 +7,10 @@ export default function Form(props) {
     setInterviewer(() => null);
     setStudent(() => "");
   };
-
+console.log("Renders Form")
   const cancel = () => {
     reset();
+    console.log("Anything")
     props.onCancel("Cancel Request");
   };
 
