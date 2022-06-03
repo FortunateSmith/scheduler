ec78237b6f34de3bf3d1680b25ecfc249e2507df
Liam <liam.s.smith@gmail.com>
2022-06-02 20:34:14 -0400

    add transition to Show and Saving, create save function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,7 +7,9 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 ///////////////////////
+
 
 import useVisualMode from "hooks/useVisualMode";
 
@@ -15,10 +17,24 @@ export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "STATUS";
   // ternary is used to set initial state of mode
-  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    console.log("*************", interview);
+    transition(SAVING);
+    props.bookInterview(props.id, interview)
+      .then(() => transition(SHOW));
+  }
 
   return (
     <article className="appointment">
@@ -26,11 +42,14 @@ export default function Appointment(props) {
 
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
-        <Show student={props.interview.student} interviewer={props.interview.interviewer} />
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
       )}
-      {mode === CREATE && (<Form interviewers={[]} 
-       onCancel={back}
-      />)}
+      {mode === CREATE && <Form interviewers={props.interviewers} onCancel={back} onSave={save} />}
+
+      {mode === SAVING && <Status />}
 
       {/* CODE BEING REPLACED:: {interview ? (
         <Show student={interview.student} interviewer={interview.interviewer} />
