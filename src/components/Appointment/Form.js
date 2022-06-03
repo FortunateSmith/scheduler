29fab580b8f1aebcdb291709af101a44e87b88d7
Liam <liam.s.smith@gmail.com>
2022-06-02 20:31:34 -0400

    add useState for student and interviewer, pass props to saveReq function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,34 +3,35 @@ import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  
   const reset = () => {
     setInterviewer(() => null);
     setStudent(() => "");
   };
-console.log("Renders Form")
+
   const cancel = () => {
     reset();
-    console.log("Anything")
     props.onCancel("Cancel Request");
   };
 
   const saveReq = () => {
-    props.onSave("Save Request");
+    props.onSave(student, interviewer);
   };
 
-  const [student, setStudent] = useState(props.student || "");
-  const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
             value={student}
+            // capture happens here!!!
             onChange={(event) => setStudent(event.target.value)}
           />
         </form>
