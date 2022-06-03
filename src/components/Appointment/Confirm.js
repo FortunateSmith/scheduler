9c561ad473189daafadbf6f71483087273c3d0ca
Liam <liam.s.smith@gmail.com>
2022-06-02 22:46:03 -0400

    add props to component from parent Appointments index

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -3,23 +3,17 @@ import Button from "components/Button";
 import "./styles.scss";
 
 export default function Confirm(props) {
-
-  const confirm = () => {
-    props.onConfirm("Confirm Delete");
-  }
-
-  const cancel = () => {
-    props.onCancel("Cancel Delete");
-  }
-    
-
-  return(
+  return (
     <main className="appointment__card appointment__card--confirm">
-  <h1 className="text--semi-bold">{props.message}</h1>
-  <section className="appointment__actions">
-    <Button danger onClick={cancel}>Cancel</Button>
-    <Button danger onClick={confirm}>Confirm</Button>
-  </section>
-</main>
-  )
+      <h1 className="text--semi-bold">{props.message}</h1>
+      <section className="appointment__actions">
+        <Button danger onClick={props.onCancel}>
+          Cancel
+        </Button>
+        <Button danger onClick={props.onConfirm}>
+          Confirm
+        </Button>
+      </section>
+    </main>
+  );
 }
